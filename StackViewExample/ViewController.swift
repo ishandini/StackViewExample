ec24c8b605f9989ab50c81628279d2beca426a8c
Ishan <ishan@app-monkeyz.com>
2016-01-02 12:39:33 +0530

    Added UIStackView

---
+++ StackViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViewExample
+//
+//  Created by Ishan on 1/2/16.
+//  Copyright © 2016 Ishan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
